9b22b99176263ad7c5cebd9c39fc67fe314a2253
satyabrat35 <satyabrat35@gmail.com>
2023-01-12 00:12:12 -0500

    Improved logic for C++ Code, Added py code which passed all cases!

---
+++ Round 1A 2019/Pylons.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Satyabrat Bhol on 12/01/23.
+//
+/**************bhol****************/
+// #include <bits/stdc++.h>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+
+}
